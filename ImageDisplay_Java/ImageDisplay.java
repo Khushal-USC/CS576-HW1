28df292cc0831ddca4874086bf826937ba253d16
Khushal-Kalidindi <kalidindikhushal@gmail.com>
2024-09-23 18:53:39 -0700

    finished bilinear

--- ImageDisplay_Java/ImageDisplay.java
+++ ImageDisplay_Java/ImageDisplay.java
@@ -151,6 +151,119 @@ public class ImageDisplay {
 
 	}
 
+	private int linInterp(double srcX, double srcY, int x1, int y1, int x2, int y2, int q11, int q12, int q21, int q22){
+		//X dir interp
+		int r1_x_y = 0;
+		if (x1 == x2) {
+			r1_x_y = q11;  // No interpolation in x direction, use q11 directly
+		} else {
+			r1_x_y = (int)(q11 *((double)(x2 - srcX)/(x2-x1)) + q21*((double)(srcX-x1)/(x2-x1)));
+		}
+
+		double r2_x_y;
+		if (x1 == x2) {
+			r2_x_y = q12;  // No interpolation in x direction, use q12 directly
+		} else {
+			r2_x_y = (int)(q12 *((double)(x2 - srcX)/(x2-x1)) + q22*((double)(srcX-x1)/(x2-x1)));
+		}
+
+		double p_x_y;
+		if (y1 == y2) {
+			p_x_y = r1_x_y;  // No interpolation in y direction, use r1 directly
+		} else {
+			p_x_y = (int)(r1_x_y *((double)(y2 - srcY)/(y2-y1)) + r2_x_y*((double)(srcY-y1)/(y2-y1)));
+		}
+		return (int)Math.min(Math.max(p_x_y,0),255);
+	}
+
+	private int getR(int color){
+		return ((color & 0xff0000) >> 16);
+	}
+	private int getG(int color){
+		return ((color & 0xff00) >> 8);
+	}
+	private int getB(int color){
+		return (color & 0xff);
+	}
+
+	private void bilinear(int width, int height,int outWidth, int outHeight, BufferedImage imgIn, BufferedImage imgOut)
+	{
+		System.out.println("doing 2");
+		int outY = 0;
+		while(outY < outHeight)
+		{
+			int outX = 0;
+			while(outX < outWidth)
+			{
+				double scaleX = (double) width / outWidth;
+				double scaleY = (double) height / outHeight;
+
+				double srcX = (outX * scaleX);
+				double srcY = (outY * scaleY);
+				int x1 = (int)srcX;
+				int x2 = Math.min(Math.max(x1 + 1,0),width-1);
+				int y1 = (int)srcY;
+				int y2 = Math.min(Math.max(y1 + 1,0),height-1);
+
+				int q11 = imgIn.getRGB(x1, y1);
+				int q12 = imgIn.getRGB(x1, y2);
+				int q21 = imgIn.getRGB(x2, y1);
+				int q22 = imgIn.getRGB(x2, y2);
+				
+				
+
+				// Components will be in the range of 0..255:
+				byte r = (byte)linInterp(srcX, srcY, x1, y1, x2, y2, getR(q11), getR(q12), getR(q21), getR(q22));
+				byte g = (byte)linInterp(srcX, srcY, x1, y1, x2, y2, getG(q11), getG(q12), getG(q21), getG(q22));
+				byte b = (byte)linInterp(srcX, srcY, x1, y1, x2, y2, getB(q11), getB(q12), getB(q21), getB(q22));
+				
+				//byte a = (byte)((color & 0xff000000) >> 24);
+
+				int pixOut = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+				//int pix = ((a << 24) + (r << 16) + (g << 8) + b);
+				//int pix = 0xff32A852;
+				imgOut.setRGB(outX,outY,pixOut);
+				outX++;
+			}
+			//System.out.println("herey " +y);
+			outY++;
+		}
+	}
+
+	private void upSample1(int width, int height,int outWidth, int outHeight, BufferedImage imgIn, BufferedImage imgOut)
+	{
+		System.out.println("doing 1");
+		int outY = 0;
+		while(outY < outHeight)
+		{
+			int outX = 0;
+			while(outX < outWidth)
+			{
+				float scaleX = (float) width / outWidth;
+				float scaleY = (float) height / outHeight;
+
+				int srcX = (int) (outX * scaleX);
+				int srcY = (int) (outY * scaleY);
+				int color = imgIn.getRGB(Math.min(width - 1, srcX), Math.min(height - 1, srcY));
+
+				// Components will be in the range of 0..255:
+				byte r = (byte)((color & 0xff0000) >> 16);
+				byte g = (byte)((color & 0xff00) >> 8);
+				byte b = (byte)(color & 0xff);
+				
+				byte a = (byte)((color & 0xff000000) >> 24);
+
+				//int pixOut = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+				int pix = ((a << 24) + (r << 16) + (g << 8) + b);
+				//int pix = 0xff32A852;
+				imgOut.setRGB(outX,outY,pix);
+				outX++;
+			}
+			//System.out.println("herey " +y);
+			outY++;
+		}
+	}
+
 
 	private void downSample1(int width, int height,int outWidth, int outHeight, BufferedImage imgIn, BufferedImage imgOut)
 	{
@@ -194,8 +307,8 @@ public class ImageDisplay {
 
 
 	public void showIms(String[] args){
-		int width = 4000;
-		int height = 3000;
+		int width = 400;
+		int height = 300;
 
 		// Read in the specified image
 		BufferedImage res = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -210,18 +323,32 @@ public class ImageDisplay {
 
 		//Downsample2
 		//GaussBlur
-		double rho = 3;
+		// double rho = 3;
 
-		BufferedImage imgBlurOut = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		gaussBlur(width, height, (int)(3*rho), rho, res, imgBlurOut);
-		res = imgBlurOut;
+		// BufferedImage imgBlurOut = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		// gaussBlur(width, height, (int)(3*rho), rho, res, imgBlurOut);
+		// res = imgBlurOut;
 		
-		//Then Ds1
-		int outWidth = 640;
-		int outHeight = 400;
-		BufferedImage ds1Out = new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_INT_RGB);
-		downSample1(width, height, outWidth, outHeight, res, ds1Out);
-		res = ds1Out;
+		// //Then Ds1
+		// int outWidth = 640;
+		// int outHeight = 400;
+		// BufferedImage ds1Out = new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_INT_RGB);
+		// downSample1(width, height, outWidth, outHeight, res, ds1Out);
+		// res = ds1Out;
+
+		// //Upsample
+		// int outWidth = 1920;
+		// int outHeight = 1080;
+		// BufferedImage us1Out = new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_INT_RGB);
+		// upSample1(width, height, outWidth, outHeight, res, us1Out);
+		// res = us1Out;
+
+		//Upsample
+		int outWidth = 1920;
+		int outHeight = 1080;
+		BufferedImage biOut = new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_INT_RGB);
+		bilinear(width, height, outWidth, outHeight, res, biOut);
+		res = biOut;
 
 		// Use label to display the image
 		frame = new JFrame();
